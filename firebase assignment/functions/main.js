2d002c18b09b69960df438db96d14b4e40a2d412
Nathaniel Burciaga <nathanielburciaga@Nathaniels-MBP.home>
2020-10-12 22:48:44 -0700

    added all project files and config

---
+++ firebase assignment/functions/main.js
@@ -0,0 +1,13 @@
+const config = {
+  apiKey: "AIzaSyB_94jwQe9a1UgeVoLaWhSltxhTrnWm2zI",
+   authDomain: "fir-app4050.firebaseapp.com",
+   databaseURL: "https://fir-app4050.firebaseio.com",
+   projectId: "fir-app4050",
+   storageBucket: "fir-app4050.appspot.com",
+   messagingSenderId: "225290224228",
+   appId: "1:225290224228:web:6f9c0bfe1ebe75406ad571"
+}
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
